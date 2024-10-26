b888bf55ad7bfe807cdbcb2f17162f847981fe22
foxx <jkoontsiii@gmail.com>
2024-10-26 19:21:49 -0400

    .

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function add_to() {
-    readarray -t FIARRY < "$1"
+    mapfile -t FIARRY < "$1"
     for LI in "${FIARRY[@]}"; do
       # shellcheck disable=SC2002
       CHECK=$(cat "$2" | grep "$LI")
