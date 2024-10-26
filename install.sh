16a35aed62c320ba863baf9d7a3d35ea148ddfa6
foxx <jkoontsiii@gmail.com>
2024-10-26 19:01:55 -0400

    .

--- install.sh
+++ install.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+LINES=$(cat bash/bashrc)
+for LI in "${LINES[@]};"; do
+  CHECK=$(cat ~/.bashrc | grep "$LI")
+  if [[ -z $CHECK ]]; then
+    echo "adding: $LI"
+    cat bash/bashrc >> ~/.bashrc
+  fi
+done
+
+#cat bash/bashrc >> ~/.bashrc
