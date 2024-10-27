226cc2aa084bace02c746e345cb799d666f3716c
foxx <jkoontsiii@gmail.com>
2024-10-26 20:05:57 -0400

    .

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 echo "installing dotfiles .."
 function add_from_to() {
+  if [[ -f bash/bashrc ]]; then
+    mapfile -t FIARRY <"$1"
+  else
     mapfile -t FIARRY< <(curl -H 'Cache-Control: no-cache' -sL https://raw.githubusercontent.com/helloimalemur/dotfiles/refs/heads/master/"$1")
+  fi
     for LI in "${FIARRY[@]}"; do
       # shellcheck disable=SC2002
       CHECK=$(cat "$2" | grep "$LI")
