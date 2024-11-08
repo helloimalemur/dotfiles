806528e2704083835a7f3814ce6bedc758f74415
helloimalemur <jkoontsiii@gmail.com>
2024-11-08 11:24:20 -0500

    .

--- install.sh
+++ install.sh
@@ -6,14 +6,15 @@ function add_from_to() {
   else
     mapfile -t FIARRY< <(curl -H 'Cache-Control: no-cache' -sL https://raw.githubusercontent.com/helloimalemur/dotfiles/refs/heads/master/"$1")
   fi
-    for LI in "${FIARRY[@]}"; do
-      # shellcheck disable=SC2002
-      CHECK=$(cat "$2" | grep "$LI")
-      if [[ -z $CHECK ]]; then
-        echo "adding: $LI"
-        echo "$LI" >> "$2"
-      fi
-    done
+
+  for LI in "${FIARRY[@]}"; do
+    # shellcheck disable=SC2002
+    CHECK=$(cat "$2" | grep "$LI")
+    if [[ -z $CHECK ]]; then
+      echo "adding: $LI"
+      echo "$LI" >> "$2"
+    fi
+  done
 }
 
 add_from_to bash/bashrc ~/.bashrc
